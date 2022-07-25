1cfd6bbe12acfb55813359af5ccb3619ffdfe400
jetfire679 <jetfire679@gmail.com>
2022-07-25 16:50:08 -0400

    adding operator to east2 namespace

--- 3.1-strimzi-operator.tf
+++ 3.1-strimzi-operator.tf
@@ -13,3 +13,19 @@ resource "helm_release" "strimzi-kafka-operator" {
 
   depends_on = [aws_eks_fargate_profile.kube-system]
 }
+
+resource "helm_release" "strimzi-kafka-operator-east-2" {
+  name = "strimzi-kafka-operator"
+
+  repository = "https://strimzi.io/charts/"
+  chart      = "strimzi-kafka-operator"
+  namespace  = "kafkaeast2"
+  version    = "0.30.0"
+
+  # set {
+  #   name  = "metrics.enabled"
+  #   value = false
+  # }
+
+  depends_on = [aws_eks_fargate_profile.kube-system]
+}

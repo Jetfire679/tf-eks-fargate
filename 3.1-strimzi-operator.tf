af3df24d358a77d2c0c78fea1b4f29d92d1de997
jetfire679 <jetfire679@gmail.com>
2022-07-26 12:25:12 -0400

    overhaul

--- 3.1-strimzi-operator.tf
+++ 3.1-strimzi-operator.tf
@@ -1,14 +1,14 @@
-# resource "helm_release" "strimzi-kafka-operator-east1" {
-#   name = "strimzi-kafka-operator"
+resource "helm_release" "strimzi-kafka-operator-east1" {
+  name = "strimzi-kafka-operator"
 
-#   repository = "https://strimzi.io/charts/"
-#   chart      = "strimzi-kafka-operator"
-#   namespace  = "kafka"
-#   create_namespace = true
-#   version    = "0.30.0"
+  repository = "https://strimzi.io/charts/"
+  chart      = "strimzi-kafka-operator"
+  namespace  = "kafka"
+  create_namespace = true
+  version    = "0.30.0"
 
-#   depends_on = [aws_eks_fargate_profile.kube-system]
-# }
+  depends_on = [aws_eks_fargate_profile.kube-system]
+}
 
 # resource "helm_release" "strimzi-kafka-operator-east2" {
 #   name = "strimzi-kafka-operator-east2"

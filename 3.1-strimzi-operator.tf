3ae8d65ab646cd440ce0576d870a2c348c669e58
jetfire679 <jetfire679@gmail.com>
2022-07-26 08:27:30 -0400

    adding local helm charts

--- 3.1-strimzi-operator.tf
+++ 3.1-strimzi-operator.tf
@@ -1,8 +1,9 @@
-resource "helm_release" "strimzi-kafka-operator" {
+resource "helm_release" "strimzi-kafka-operator-east1" {
   name = "strimzi-kafka-operator"
 
   repository = "https://strimzi.io/charts/"
-  chart      = "strimzi-kafka-operator"
+  # chart      = "strimzi-kafka-operator"
+  chart      = "./kube/strimzi-kafka-operator-east1"
   namespace  = "kafkaeast1"
   version    = "0.30.0"
 
@@ -14,18 +15,18 @@ resource "helm_release" "strimzi-kafka-operator" {
   depends_on = [aws_eks_fargate_profile.kube-system]
 }
 
-resource "helm_release" "strimzi-kafka-operator-east-2" {
-  name = "strimzi-kafka-operator-2"
+# resource "helm_release" "strimzi-kafka-operator-east-2" {
+#   name = "strimzi-kafka-operator-2"
 
-  repository = "https://strimzi.io/charts/"
-  chart      = "strimzi-kafka-operator"
-  namespace  = "kafkaeast2"
-  version    = "0.30.0"
+#   repository = "https://strimzi.io/charts/"
+#   chart      = "strimzi-kafka-operator"
+#   namespace  = "kafkaeast2"
+#   version    = "0.30.0"
 
-  # set {
-  #   name  = "metrics.enabled"
-  #   value = false
-  # }
+#   # set {
+#   #   name  = "metrics.enabled"
+#   #   value = false
+#   # }
 
-  depends_on = [aws_eks_fargate_profile.kube-system]
-}
+#   depends_on = [aws_eks_fargate_profile.kube-system]
+# }

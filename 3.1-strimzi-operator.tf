eab908352d99ed21b51c020160375bcf9cf20e8f
jetfire679 <jetfire679@gmail.com>
2022-07-26 09:50:35 -0400

    setting create_namespace to true as part of helm release

--- 3.1-strimzi-operator.tf
+++ 3.1-strimzi-operator.tf
@@ -12,15 +12,15 @@
 # }
 
 
-resource "helm_release" "strimzi-kafka-operator-east2" {
-  name = "strimzi-kafka-operator-east2"
+# resource "helm_release" "strimzi-kafka-operator-east2" {
+#   name = "strimzi-kafka-operator-east2"
 
-  repository = "https://strimzi.io/charts/"
-  # chart      = "strimzi-kafka-operator"
-  chart      = "./kube/strimzi-kafka-operator-east2"
-  namespace  = "kafkaeast2"
-  create_namespace = true
-  version    = "0.30.0"
+#   repository = "https://strimzi.io/charts/"
+#   # chart      = "strimzi-kafka-operator"
+#   chart      = "./kube/strimzi-kafka-operator-east2"
+#   namespace  = "kafkaeast2"
+#   create_namespace = true
+#   version    = "0.30.0"
 
-  depends_on = [aws_eks_fargate_profile.kube-system]
-}
+#   depends_on = [aws_eks_fargate_profile.kube-system]
+# }

0fadfd65b3d20a9cb13e927fd5771ab09081f2c1
jetfire679 <jetfire679@gmail.com>
2022-07-26 14:41:30 -0400

    adding alb

--- 2.0.6-alb-controller.tf
+++ 2.0.6-alb-controller.tf
@@ -34,7 +34,7 @@ resource "helm_release" "aws-load-balancer-controller" {
   # EKS Fargate specific
   set {
     name  = "region"
-    value = "us-east-1"
+    value = var.AWS_DEFAULT_REGION
   }
 
   set {

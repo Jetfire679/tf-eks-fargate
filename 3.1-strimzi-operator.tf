183d69787774a9e2eb1c58b0f7fa46ab996c5bf6
jetfire679 <jetfire679@gmail.com>
2022-07-26 09:00:17 -0400

    adding local helm charts2

--- 3.1-strimzi-operator.tf
+++ 3.1-strimzi-operator.tf
@@ -7,26 +7,24 @@ resource "helm_release" "strimzi-kafka-operator-east1" {
   namespace  = "kafkaeast1"
   version    = "0.30.0"
 
-  # set {
-  #   name  = "metrics.enabled"
-  #   value = false
-  # }
-
   depends_on = [aws_eks_fargate_profile.kube-system]
 }
 
-# resource "helm_release" "strimzi-kafka-operator-east-2" {
-#   name = "strimzi-kafka-operator-2"
 
-#   repository = "https://strimzi.io/charts/"
-#   chart      = "strimzi-kafka-operator"
-#   namespace  = "kafkaeast2"
-#   version    = "0.30.0"
+resource "kubernetes_namespace" "kafkaeast2" {
+  metadata {
+    name = "kafkaeast2"
+  }
+}
 
-#   # set {
-#   #   name  = "metrics.enabled"
-#   #   value = false
-#   # }
+resource "helm_release" "strimzi-kafka-operator-east2" {
+  name = "strimzi-kafka-operator-east2"
 
-#   depends_on = [aws_eks_fargate_profile.kube-system]
-# }
+  repository = "https://strimzi.io/charts/"
+  # chart      = "strimzi-kafka-operator"
+  chart      = "./kube/strimzi-kafka-operator-east2"
+  namespace  = "kafkaeast1"
+  version    = "0.30.0"
+
+  depends_on = [kubernetes_namespace.kafkaeast2]
+}

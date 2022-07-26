d72a09135e265fc2b8578521a9b1bcfb61bdf208
jetfire679 <jetfire679@gmail.com>
2022-07-26 09:35:32 -0400

    setting create_namespace to true as part of helm release

--- 3.1-strimzi-operator.tf
+++ 3.1-strimzi-operator.tf
@@ -18,7 +18,7 @@ resource "helm_release" "strimzi-kafka-operator-east2" {
   repository = "https://strimzi.io/charts/"
   # chart      = "strimzi-kafka-operator"
   chart      = "./kube/strimzi-kafka-operator-east2"
-  namespace  = "kafkaeast1"
+  namespace  = "kafkaeast2"
   create_namespace = true
   version    = "0.30.0"
 
